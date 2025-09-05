ef55f3e806d7a10a6a3ae275393bb69d36c2ce8b
Mariana <marangob@udistrital.edu.co>
2025-09-04 21:41:38 -0500

    Prueba

---
+++ Capitulo 1.R
@@ -0,0 +1,12 @@
+setwd("C:/Users/maria/OneDrive/Documentos/1. Universidad/8 Semestre/Econometria/Tareas R")
+
+Hali
+a = list(1,2,3) #Para hacer una lista con diferentes tipos de variables.
+a = as.matrix(a) #Pasa a convertir los datos de a en una matriz 3x1 (Depende de los datos)
+
+b <- 34 #Alt (-) puedo hacer una asignacion de variable
+
+
+#Para hacer una matriz 
+a <- list (1:5)
+a <- as.matrix()
